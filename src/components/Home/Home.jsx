dff8a4ee44b838a7195e7a6981e01e9901dea8b4
liton61 <litonofficial61@gmail.com>
2023-11-10 13:56:59 +0600

    online order added

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,10 +1,12 @@
 import Slider from "../Header/Slider";
+import OnlineOrder from "./OnlineOrder";
 
 
 const Home = () => {
     return (
         <div>
            <Slider></Slider>
+           <OnlineOrder></OnlineOrder>
         </div>
     );
 };
