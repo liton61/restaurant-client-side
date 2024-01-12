8e697f7c7f9d47eabaa0109bab5dd9ecdd422f1d
liton61 <litonofficial61@gmail.com>
2024-01-12 16:49:54 +0600

    border added to menu banner

--- src/components/MenuBanner/MenuBanner.jsx
+++ src/components/MenuBanner/MenuBanner.jsx
@@ -7,7 +7,7 @@ const MenuBanner = () => {
             <div className='my-20 bg-image lg:px-0 md:px-5 px-5'>
                 <div className='lg:flex items-center lg:w-3/4 md:w-full w-full mx-auto py-20'>
                     <div className='lg:w-1/2'>
-                        <img className='rounded border border-gray-800' src={banner} alt="" />
+                        <img className='rounded border border-yellow-600' src={banner} alt="" />
                     </div>
                     <div className='lg:ml-10 lg:w-1/2'>
                         <h3 className='text-2xl font-semibold'>March 20, 2023 <br />WHERE CAN I GET SOME?</h3>
