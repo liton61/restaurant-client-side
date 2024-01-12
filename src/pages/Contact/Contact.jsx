7e7973f943ebf8be8755e81c2c4b1a1f3399279a
liton61 <litonofficial61@gmail.com>
2024-01-12 12:47:32 +0600

    responsive design added for contact page

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -22,59 +22,61 @@ const Contact = () => {
     };
 
     return (
-        <div className="max-w-4xl mx-auto bg-gray-200 p-10 my-10 rounded">
-            <div className='flex justify-center'>
-                <div className="stats stats-vertical lg:stats-horizontal md:stats-horizontal shadow">
+        <div className='lg:px-0 px-5'>
+            <div className="lg:w-3/4 mx-auto bg-gray-200 p-10 my-10 rounded">
+                <div className='flex justify-center'>
+                    <div className="stats stats-vertical lg:stats-horizontal md:stats-horizontal shadow">
 
-                    <div className="stat">
-                        <div className="stat-title">Location</div>
-                        <div className="stat-desc">Dhaka, Bangladesh</div>
-                    </div>
-                    <div className="stat">
-                        <div className="stat-title">Phone</div>
-                        <div className="stat-desc">+123 456 7890</div>
-                    </div>
-                    <div className="stat">
-                        <div className="stat-title">Email</div>
-                        <div className="stat-desc">info@example.com</div>
+                        <div className="stat">
+                            <div className="stat-title">Location</div>
+                            <div className="stat-desc">Dhaka, Bangladesh</div>
+                        </div>
+                        <div className="stat">
+                            <div className="stat-title">Phone</div>
+                            <div className="stat-desc">+123 456 7890</div>
+                        </div>
+                        <div className="stat">
+                            <div className="stat-title">Email</div>
+                            <div className="stat-desc">info@example.com</div>
+                        </div>
                     </div>
                 </div>
+                <form ref={form} onSubmit={sendEmail}>
+                    <div className="mb-4">
+                        <label htmlFor="name" className="block mb-2 text-lg">Name</label>
+                        <input
+                            type="text"
+                            id="name"
+                            name="user_name"
+                            className="w-full px-4 py-2 rounded-md text-black focus:outline-none"
+                            required placeholder='Your name'
+                        />
+                    </div>
+                    <div className="mb-4">
+                        <label htmlFor="email" className="block mb-2 text-lg">Email</label>
+                        <input
+                            type="email"
+                            id="email"
+                            name="user_email"
+                            className="w-full px-4 py-2 rounded-md text-black focus:outline-none"
+                            required placeholder='Your email'
+                        />
+                    </div>
+                    <div className="mb-4">
+                        <label htmlFor="message" className="block mb-2 text-lg">Message</label>
+                        <textarea
+                            id="message"
+                            name="message"
+                            className="w-full px-4 py-2 rounded-md text-black focus:outline-none"
+                            rows="5"
+                            required placeholder='Message'
+                        ></textarea>
+                    </div>
+                    <button className='btn btn-success font-bold w-full'>
+                        Submit
+                    </button>
+                </form>
             </div>
-            <form ref={form} onSubmit={sendEmail}>
-                <div className="mb-4">
-                    <label htmlFor="name" className="block mb-2 text-lg">Name</label>
-                    <input
-                        type="text"
-                        id="name"
-                        name="user_name"
-                        className="w-full px-4 py-2 rounded-md text-black focus:outline-none"
-                        required placeholder='Your name'
-                    />
-                </div>
-                <div className="mb-4">
-                    <label htmlFor="email" className="block mb-2 text-lg">Email</label>
-                    <input
-                        type="email"
-                        id="email"
-                        name="user_email"
-                        className="w-full px-4 py-2 rounded-md text-black focus:outline-none"
-                        required placeholder='Your email'
-                    />
-                </div>
-                <div className="mb-4">
-                    <label htmlFor="message" className="block mb-2 text-lg">Message</label>
-                    <textarea
-                        id="message"
-                        name="message"
-                        className="w-full px-4 py-2 rounded-md text-black focus:outline-none"
-                        rows="5"
-                        required placeholder='Message'
-                    ></textarea>
-                </div>
-                <button className='btn btn-success font-bold w-full'>
-                    Submit
-                </button>
-            </form>
         </div>
     );
 };
