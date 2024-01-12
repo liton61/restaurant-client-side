959fe3d2cf70095e277e231bd5a85162e379c0d2
liton61 <litonofficial61@gmail.com>
2024-01-12 11:45:20 +0600

    responsive design added for order page

--- src/pages/Order/OrderTab.jsx
+++ src/pages/Order/OrderTab.jsx
@@ -4,7 +4,7 @@ import FoodCard from "../../components/FoodCard/FoodCard";
 
 const OrderTab = ({ items }) => {
     return (
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 lg:w-3/4 w-full mx-auto lg:px-0 md:px-0 px-5">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 lg:w-3/4 w-full mx-auto gap-5 lg:px-0 px-5 mb-10">
             {
                 items.map(item => <FoodCard key={item._id} item={item}></FoodCard>)
             }
